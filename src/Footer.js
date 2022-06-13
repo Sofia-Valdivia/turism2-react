6d9e2308e4c411aba74d7d457d1cff6c65ad1c3a
Sofia-Valdivia <bastet.svt@gmail.com>
2022-06-13 00:24:26 -0500

    react-turismo

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+const Footer = () => {
+
+    return(
+        <div>
+            <footer>
+            <h2>Copyright ©2022</h2>
+            </footer>
+        </div>
+
+    );
+};
+
+export default Footer
---
+++ src/Header.js
@@ -0,0 +1,20 @@
+
+const Header = () => {
+
+    return(
+        <div>
+            <header>
+                <nav>
+                    <ul>
+                        <li>Nosotros</li>
+                        <li>Galeria</li>
+                        <li>Contacto</li>
+                    </ul>
+                </nav>
+            </header>
+        </div>
+
+    );
+};
+
+export default Header
